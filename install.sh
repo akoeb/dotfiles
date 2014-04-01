be719299f4a3421d13362d4b453221090f8a1350
Alexander Köb <nerdkram@koeb.me>
2014-04-01 19:58:45 +0200

    finishing install routine

--- install.sh
+++ install.sh
@@ -12,11 +12,16 @@ fi
 # check existence of basename command
 if [ -z "$(which basename)" ]
 then
-    echo "Basename command is missing, vannot proceed"
+    echo "Basename command is missing, cannot proceed"
     exit 1
 fi
 
-# TODO check existance of sha1sum
+# check existance of sha1sum
+if [ -z "$(which sha1sum)" ]
+then
+    echo "sha1sum checksum command is missing, cannot proceed"
+fi
+
 
 # function to copy is checksum is not changed. makes also backup
 function copy() {
@@ -27,15 +32,18 @@ function copy() {
     if [ "$(sha1sum $from | awk '{print $1}' )" == "$(sha1sum $to | awk '{print $1}')" ] 
     then
         # files are identical
+        echo "Files $from and $to are identical, skipping."
         return
     fi
 
-    backup_file=${to}.$( date +%Y%m%d%H%M )
+    backup_file=$(dirname $to)/${to#$(dirname $to)/.}.$( date +%Y%m%d%H%M )
+    echo "Overwriting file $to, original is backed up to $backup_file (consider updating the dotfile repo)"
     cp $to $backup_file
     cp $from $to
 }
 
-# TODO cd into the base directory of the repository
+# cd into the base directory of the repository
+cd $(dirname ${PWD}/$0)
 
 # copy rcfiles
 for file in rcfiles/*
